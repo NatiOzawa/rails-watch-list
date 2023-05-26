9f0a2100ee99f7eb1f250c14b585e79c9624b4fd
Natalia Ozawa <nati.ozawa@gmail.com>
2023-05-26 12:05:18 -0300

    almost done

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,34 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @bookmark
+      if bookmark.save
+        redirect_to list_path(list)
+      else
+        render "lists/show", status: unprocessable_entity
+      end
+    end
+
+  def create
+    list = List.find(params[:list_id])
+    movie = Movie.find(params[:bookmark][:movie])
+    bookmark = Bookmark.new(bookmark_params)
+    bookmark.list = list
+    bookmark.movie = movie
+    bookmark.save
+    redirect_to list_path(list)
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,20 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  # root "articles#insdex"
   # root "articles#index"
+
+  # get "/lists", to: "lists#index"
+  # get "/lists/new", to: "lists#new"
+  # post "/lists", to: "lists#create"
+  # get "/lists/:id", to: "lists#show", as: "list"
+  # get "/lists/by_name/:name", to: "lists#name", as: "list_by_name"
+  # get "/lists/:id/edit", to: "lists#edit", as: "edit_list"
+  # patch "/lists/:id", to: "lists#update", as: "update_list"
+  # delete "/lists/:id", to: "lists#destroy"
+
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
